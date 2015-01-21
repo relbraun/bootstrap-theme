40f5c02c36e73b375765c14b79293635be384d74
unknown <Arielb@600G1TWR-APP-C13.ravtech.local>
2015-01-21 13:39:49 +0200

    push

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -7,12 +7,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-    <script src="<?php echo Yii::app()->theme->baseUrl . "/js/jquery.js"; ?>"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?php echo Yii::app()->theme->baseUrl . "/js/bootstrap.min.js"; ?>"></script>
-    <!-- Bootstrap -->
-    <link href="<?php echo Yii::app()->theme->baseUrl . "/css/bootstrap.min.css"; ?>" rel="stylesheet">
+      <script src="<?php echo Yii::app()->theme->baseUrl . "/js/jquery.js"; ?>"></script>
+
+      <!-- Bootstrap -->
+      <link href="<?php echo Yii::app()->theme->baseUrl . "/css/bootstrap.min.css"; ?>" rel="stylesheet">
+      <link href="<?php echo Yii::app()->theme->baseUrl . "/css/style.css"; ?>" rel="stylesheet">
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -58,4 +59,6 @@
     <?php echo $content; ?>
 
   </body>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="<?php echo Yii::app()->theme->baseUrl . "/js/bootstrap.min.js"; ?>"></script>
 </html>
